997ff2dfcaa22356226f428c5644f5269e2b8e33
alex.junior <alex.junior@RSI1521.rsinet.com.br>
2020-02-11 16:40:44 -0300

    Step de teste para cadastro de usuário.

--- HubTalentos-Avaliacao-Appium_BDD/src/test/java/br/com/rsinet/appium/stepdefinitions/CadastroUsuario_Step.java
+++ HubTalentos-Avaliacao-Appium_BDD/src/test/java/br/com/rsinet/appium/stepdefinitions/CadastroUsuario_Step.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
@@ -19,14 +18,14 @@ import br.com.rsinet.appium.utils.Constant;
 import br.com.rsinet.appium.utils.DriverFactory;
 import br.com.rsinet.appium.utils.ExcelUtils;
 import br.com.rsinet.appium.utils.ExtentReport;
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.E;
+import cucumber.api.java.pt.Entao;
+import cucumber.api.java.pt.Mas;
+import cucumber.api.java.pt.Quando;
 import io.appium.java_client.android.AndroidDriver;
-import io.cucumber.java.pt.Dado;
-import io.cucumber.java.pt.E;
-import io.cucumber.java.pt.Entao;
-import io.cucumber.java.pt.Quando;
 
-
-public class CadastroUsuarioStep {
+public class CadastroUsuario_Step {
 	private AndroidDriver<WebElement> driver;
 	private RegisterScreen registerScreen;
 	private HomeScreen homeScreen;
@@ -36,25 +35,23 @@ public class CadastroUsuarioStep {
 	private ExtentTest report;
 	static ExtentReports test;
 
-	@Before
-	public void Inicializa() throws Exception {
-		registerScreen = PageFactory.initElements(driver, RegisterScreen.class);
-		loginScreen = PageFactory.initElements(driver, LoginScreen.class);
-		homeScreen = PageFactory.initElements(driver, HomeScreen.class);
-		autoCompleteRegisterUser = new AutoCompleteRegisterUser(driver);
-		ExcelUtils.setExcelFile(Constant.File_DataUserRegister, "Users");
-	}
+
 
 	@After
 	public void Finaliza() {
 
 		driver.quit();
 	}
-	
 
 	@Dado("^que estou na homepage do aplicativo$")
 	public void que_estou_na_homepage_do_aplicativo() throws Exception {
 		driver = DriverFactory.getDriver();
+		registerScreen = PageFactory.initElements(driver, RegisterScreen.class);
+		loginScreen = PageFactory.initElements(driver, LoginScreen.class);
+		homeScreen = PageFactory.initElements(driver, HomeScreen.class);
+		autoCompleteRegisterUser = new AutoCompleteRegisterUser(driver);
+		ExcelUtils.setExcelFile(Constant.File_DataUserRegister, "Users");
+
 	}
 
 	@Quando("^clico no icone de login e em criar nova conta$")
@@ -87,4 +84,26 @@ public class CadastroUsuarioStep {
 		teste = "TesteCadastroNovoUsuarioComSucesso";
 
 	}
+
+	@E("^preencho os campos de detalhe da Conta$")
+	public void preencho_os_campos_de_detalhe_da_Conta() throws Throwable {
+		registerScreen.setUserName(autoCompleteRegisterUser.getUserName(7));
+		registerScreen.setEmail(ExcelUtils.getCellData(4, 2));
+
+	}
+
+	@Mas("^insiro uma senha sem letra maiuscula$")
+	public void insiro_uma_senha_sem_letra_maiuscula() throws Throwable {
+		registerScreen.setPassword(ExcelUtils.getCellData(4, 3));
+		registerScreen.setConfirmPassword(ExcelUtils.getCellData(4, 3));
+	}
+
+	@Entao("^recebo uma mensagem de requisicao$")
+	public void recebo_uma_mensagem_de_requisicao() throws Throwable {
+		Assert.assertEquals("1 upper letter required", registerScreen.getIconPassword());
+
+		ExtentReport.statusReported(report, driver, teste);
+		teste = "TesteCadastroNovoUsuarioComFalha";
+	}
+
 }
